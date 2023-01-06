1fcde8866ae05beb532eaeac226e2e9b70c880e7
Aastha Pinhatiya <94792638+aasthalovespasta@users.noreply.github.com>
2023-01-07 00:23:49 +0530

    Create addfirst.java

---
+++ Linked_list/addfirst.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    
+
+    public void addFirst(int val) {
+      // write your code here
+      Node temp = new Node();
+      temp.data=val;
+      temp.next=head;
+      head=temp;
+      if(size==0){
+          tail=temp;
+      }
+      size++;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LinkedList list = new LinkedList();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("addLast")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addLast(val);
+      } else if (str.startsWith("size")) {
+        System.out.println(list.size());
+      } else if (str.startsWith("display")) {
+        list.display();
+      } else if (str.startsWith("removeFirst")) {
+        list.removeFirst();
+      } else if (str.startsWith("getFirst")) {
+        int val = list.getFirst();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getLast")) {
+        int val = list.getLast();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getAt")) {
+        int idx = Integer.parseInt(str.split(" ")[1]);
+        int val = list.getAt(idx);
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("addFirst")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addFirst(val);
+      }
+      str = br.readLine();
+    }
+  }
+}
